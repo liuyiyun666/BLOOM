afc762e026051af4250c21b20f528c4e55f27bb7
meilishuo <271771003@qq.com>
2017-09-19 17:03:48 +0800

    dddd

---
+++ src/components/public/header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+      <slot>内容</slot>
+  </header>
+</template>
+
+<script>
+    
+</script>
+
+<style lang="less" scoped>
+  header{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 0.9rem;
+    background: #fff;
+    padding: 0 0.24rem;
+    display: flex;
+    box-sizing: border-box;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
